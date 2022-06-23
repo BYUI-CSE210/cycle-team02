ed3ab64be8c52b3a10ab2cf260f46b1e85d62a75
Nathan Rolim <nathansrolim@gmail.com>
2022-06-23 01:58:50 -0300

    update

--- cycle/__main__.py
+++ cycle/__main__.py
@@ -8,24 +8,31 @@ from trail import Trail
 from handle_collision_action import HandleCollisionsAction
 from video_services import VideoServices
 
-
-
- # Initialize pygame
+# Initialize pygame
 pygame.init()
-
-#Initialize music mixer
+#width = 800
+#height = 800
+#window = pygame.display.set_mode((width,height))
+#bg_img = pygame.image.load('byui-blackbox.jpg')
+#bg_img = pygame.transform.scale(bg_img,(width,height))
+ 
 mixer.init()
-#mixer.music.load('bensound-summer_ogg_music.wav')
-#mixer.music.play()
-
+mixer.music.load('cycle/audio/bensound-summer_mp3_music.wav')
+mixer.music.play()
+ 
+#runing = True
+#while runing:
+#    window.blit(bg_img,(0,0))
+#    for event in pygame.event.get():
+#        if event.type == QUIT:
+#
+#            runing = False
+#    pygame.display.update()
+#pygame.quit()
 
 def main():
     video_services = VideoServices()
     director = Director()
-
-
-
-
 
 if __name__ == "__main__":
     main()
